a4e7c0bb0b14201fdd41619bdcc4aa5b5bb734a1
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-20 14:17:56 -0300

    Método PUT

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -39,4 +39,16 @@ public class ProductController {
       return product0.<ResponseEntity<Object>>map(productModel -> ResponseEntity.status(HttpStatus.OK).body(productModel)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found."));
   }
 
+  @PutMapping("/products/{id}")
+  public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                              @RequestBody @Valid ProductRecordDto productRecordDto) {
+    Optional<ProductModel> product0 = productRepository.findById(id);
+    if(product0.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+    }
+    var productModel = product0.get();
+    BeanUtils.copyProperties(productRecordDto, productModel);
+    return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+  }
+
 }
