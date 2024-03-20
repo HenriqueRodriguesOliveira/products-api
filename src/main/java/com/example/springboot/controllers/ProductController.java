d08f07548e78ce2a81130bcccfa6d5c17f824889
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-20 12:52:40 -0300

    Mapeando o DTO / Records

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+  @Autowired
+  ProductRepository productRepository;
+
+
+}
--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -1,5 +1,12 @@
 package com.example.springboot.repositories;
 
-public interface ProductRepository {
+import com.example.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 
 }
